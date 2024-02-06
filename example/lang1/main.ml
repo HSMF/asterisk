8ff8565d95c7f20721c02fa3fed5514307832256
Conradin Laux <conradin.laux@gmail.com>
2024-02-06 17:44:47 +0100

    feat: offload error reporting to user

--- example/lang1/main.ml
+++ example/lang1/main.ml
@@ -15,11 +15,22 @@ let () =
   let f = open_in prog in
   let cont = In_channel.input_all f in
   close_in f;
-  printf "%s\n" cont;
-  cont
-  |> Lex.lex
-  |> inspect (fun tok -> printf "%s\n" (Lex.string_of_token tok))
-  |> Grammar.parse
-  |> List.map Ast.fdecl_to_string
-  |> String.concat "\n\n"
-  |> printf "%s\n"
+  try
+    cont
+    |> Lex.lex
+    |> inspect (fun tok ->
+      printf "%s\n" (Lex.string_of_token tok);
+      flush stdout)
+    |> Grammar.parse
+    |> List.map Ast.fdecl_to_string
+    |> String.concat "\n\n"
+    |> print_endline
+  with
+  | Grammar.Parse_error (Grammar.ErrUnexpectedToken (expected, state, input)) -> begin
+    eprintf
+      "expected one of: %s in state %s.\nRemaining input is: %s\n"
+      (Ast.sl Fun.id ", " expected)
+      state
+      (Ast.sl Lex.string_of_token "," input);
+    exit ~-1
+  end
